f01ff3fa10c340cc2e4069f1559486afe2db41e5
lipeeeee <filipe.pereirafga@gmail.com>
2022-12-08 15:17:27 +0000

    Diagonal A*

--- astar_pathfinding/aStarPathfinding.cs
+++ astar_pathfinding/aStarPathfinding.cs
@@ -1,4 +1,8 @@
-﻿namespace astar_pathfinding
+﻿using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.ApplicationServices;
+using System.Xml.Linq;
+
+namespace astar_pathfinding
 {
     public class aStarPathfinding
     {
@@ -15,7 +19,7 @@
             public int g; // g = distance from starting node (g cost)
             public int h; // h = how far away node is from end node (heuristic)
             public int f; // f = g + h
-            public unsafe Node? parent; // parent node
+            public Node? parent; // parent node
 
             public Node()
             {
@@ -108,27 +112,123 @@
                 neighbours = getNeighbours(lowestCost);
                 for (int i = 0; i < neighbours.Count; i++)
                 {
-                    if (open.Contains(neighbours[i]) || close.Contains(neighbours[i]))
-                    {
-                        continue;
-                    }
-
-                    open.Add(neighbours[i]);
+                    if (!open.Contains(neighbours[i]) && !close.Contains(neighbours[i]))
+                        open.Add(neighbours[i]);
                 }
             }
 
             return false;
         }
 
-        private Node calculateCost(int[] ij, Node? parent)
+        public bool getDiagonalPath()
         {
-            Node node = new()
+            int newG, dx, dy;
+            bool newPath;
+            Node lowestCost;
+            Node? current;
+            List<Node> neighbours, open = new()
             {
-                ij = ij,
-                h = getHCost(ij),
-                g = getGCost(ij),
-                parent = parent
-            };
+                // Add start node to open list
+                new()
+                {
+                    f = 0,
+                    h = 0,
+                    g = 0,
+                    ij = start_ij,
+                    parent = null
+                }
+            }, close = new();
+
+            while (open.Count > 0)
+            {
+                // Get lowest f cost in open list
+                lowestCost = getLowestFCost(open);
+
+                _ = open.Remove(lowestCost);
+                close.Add(lowestCost);
+
+                // Found end
+                if (lowestCost.ij[0] == end_ij[0] && lowestCost.ij[1] == end_ij[1])
+                {
+                    // color explored nodes
+                    foreach (Node node in close)
+                    {
+                        matrix[node.ij[0], node.ij[1]] = globals.MATRIX_VALUES["explored"];
+                    }
+                    foreach (Node node in open)
+                    {
+                        matrix[node.ij[0], node.ij[1]] = globals.MATRIX_VALUES["explored"];
+                    }
+
+                    // color path
+                    current = lowestCost;
+                    while (current != null)
+                    {
+                        matrix[current.ij[0], current.ij[1]] = globals.MATRIX_VALUES["path"];
+                        current = current.parent;
+                    }
+
+                    return true;
+                }
+
+                // Get neighbours
+                neighbours = getDiagonalNeighbours(lowestCost);
+                for (int i = 0; i < neighbours.Count; i++)
+                {
+                    if (close.Contains(neighbours[i]))
+                        continue;
+
+                    newPath = false;
+                    dx = lowestCost.ij[0] - neighbours[i].ij[0];
+                    dy = lowestCost.ij[1] - neighbours[i].ij[1];
+                    newG = (dx != 0 && dy != 0) ? lowestCost.g + 14 : lowestCost.g + 10;
+                    if (open.Contains(neighbours[i]))
+                    {
+                        if (newG < neighbours[i].g)
+                        {
+                            neighbours[i].g = newG;
+                            newPath = true;
+                        }   
+                    }
+                    else
+                    {
+                        neighbours[i].g = newG;
+                        newPath = true;
+                        open.Add(neighbours[i]);
+                    }
+
+                    if (newPath)
+                    {
+                        neighbours[i].h = (int)Math.Round(getEuclideanHCost(neighbours[i].ij));
+                        neighbours[i].f = neighbours[i].g + neighbours[i].h;
+                        neighbours[i].parent = lowestCost;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private Node calculateCost(int[] ij, Node? parent, bool diagonal = false)
+        {
+            Node node;
+
+            if (diagonal)
+                node = new()
+                {
+                    ij = ij,
+                    h = (int)Math.Round(getEuclideanHCost(ij)),
+                    g = getGCost(ij),
+                    parent = parent
+                };
+            else
+                node = new()
+                {
+                    ij = ij,
+                    h = getHCost(ij),
+                    g = getGCost(ij),
+                    parent = parent
+                };
 
             node.f = node.g + node.h;
             return node;
@@ -151,6 +251,40 @@
             neighbours = removeWalls(neighbours);
 
             return neighbours;
+        }
+
+        private List<Node> getDiagonalNeighbours(Node node)
+        {
+            List<Node> neighbours = new();
+            int[] ij = node.ij;
+
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    if (i == 0 && j == 0)
+                        continue;
+                    neighbours.Add(calculateCost(new int[] { ij[0] + i, ij[1] + j }, node, true));
+                }
+            }
+
+            // normalize and remove duplicates
+            neighbours = removeDuplicateNodes(neighbours);
+            neighbours = normalizeNodes(neighbours);
+            neighbours = removeWalls(neighbours);
+
+            return neighbours;
+        }
+
+        private double getEuclideanHCost(int[] ij)
+        {
+            int HxDiff = Math.Abs(end_ij[0] - ij[0]);
+            int HyDiff = Math.Abs(end_ij[1] - ij[1]);
+            int hCost = HxDiff + HyDiff;
+            return hCost;
+            /*int dx = Math.Abs(ij[0] - end_ij[0]);
+            int dy = Math.Abs(ij[1] - end_ij[1]);
+            return 10 * Math.Sqrt(dx * dx + dy * dy);*/
         }
 
         private int getHCost(int[] ij)
